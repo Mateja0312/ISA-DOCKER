78879678b1edc1569d286ad79b69d2403610187f
Mateja0312 <mateja.miletic5@gmail.com>
2023-05-08 18:28:05 +0200

    dockerized project

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -3,7 +3,7 @@ import {Sequelize} from 'sequelize-typescript';
 export const sequelize = new Sequelize({
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
-  "host": process.env.DB_HOST,
+  host: 'mysql_server',
   dialect: 'mysql',
   database: process.env.DB_NAME,
   storage: ':memory:',
